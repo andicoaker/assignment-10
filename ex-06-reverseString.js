12eba29fa5b763b93fe0bf9e4ea9c8bf25a151aa
andicoaker <Andi@Andreas-MacBook.local>
2017-01-26 16:33:24 -0500

    solved ex-06, starting ex-07

--- ex-06-reverseString.js
+++ ex-06-reverseString.js
@@ -5,14 +5,24 @@
  * Write a function called reverseString that takes a string as input
  * and returns a string with the characters in reverse order.
  * (note: can't use .reverse() method )
- * 
+ *
 */
 
+// INPUT: string
 
+var reverseString = function (string){
 
+  var newString = [ ]
 
+  for (var i = 0; i <= string.length; i++) {
+    // console.log(string[i]);
+    newString.push(string.charAt(string.length - i));
 
+  }
+   return newString.join("");
+}
 
+// OUTPUT: string in reverse -
 
 //*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*~*~*~*~
 console.assert( reverseString('books') === 'skoob')
